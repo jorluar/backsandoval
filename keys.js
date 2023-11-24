0c27efbbdadcf1f68e95cae171fbd7f9d26d6ad5
jorluar <jorluarx@gmail.com>
2023-11-24 14:12:22 -0600

    Cambio de datos de conexion a railway

--- keys.js
+++ keys.js
@@ -2,11 +2,18 @@
 /*const vhost = process.env.HRKU_HOST || 'localhost'
 const vuser = process.env.HRKU_USR || 'root'
 const vpasw = process.env.HRKU_PWD || 'BDcomsandoval23*!'
-const vdatb = process.env.HRKU_DB || 'dbsandoval'*/
+const vdatb = process.env.HRKU_DB || 'dbsandoval'
 const vhost = 'containers-us-west-148.railway.app'
 const vuser = 'cafe'
 const vpasw = '6nhJOvtznYDASaGEYhmP23'
+const vdatb = 'railway'*/
+
+//--Parametros de Railway
+const vhost = 'monorail.proxy.rlwy.net'
+const vuser = 'root'
+const vpasw = 'eHFEEg4-Had-6bFedc35f1fE4GcBcHa2'
 const vdatb = 'railway'
+const vport = 54346
 
 module.exports = {
     database: {
@@ -17,7 +24,7 @@ module.exports = {
         user: vuser,
         password: vpasw,
         database: vdatb,
-        port:6659
+        port: vport
     }
 };
 
